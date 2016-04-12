918f4f9a8b1d4fb14d558e939ee3da1c8978c07c
Hackbright Student <info@hackbrightacademy.com>
2016-04-11 18:05:09 -0700

    CR completed - improved comments

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -8,16 +8,16 @@ for line in file_name:
     line = line.rstrip()
     tokens = line.split(":")
  
-    key = tokens[0]
-    value = tokens[1]
+    restaurant = tokens[0]
+    rating = tokens[1]
 
-    restaurant_scores[key] = value
-#assigning key and value to the dictionary
+    restaurant_scores[restaurant] = rating
+#binding value to the dictionary
 
 for restaurant, rating in sorted(restaurant_scores.items()):
     print "{} is rated at {}".format(restaurant, rating) 
 
-#row 16 Unpacking key value into a tuple and sorting it
+#row 16 Unpacking key value into a tuple and sorting it by the first index
 
 
 
